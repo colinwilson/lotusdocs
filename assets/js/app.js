fb5d4ac9fdbe7c2a218e4949567fe4ff014e5624
Colin Wilson <colin.wilson@protonmail.ch>
2022-12-19 11:59:55 +0000

    🐞 fix broken landing page (mobile) toggle menu

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Toggle menu
+function toggleMenu() {
+    document.getElementById('isToggle').classList.toggle('open');
+    var isOpen = document.getElementById('navigation')
+    if (isOpen.style.display === "block") {
+        isOpen.style.display = "none";
+    } else {
+        isOpen.style.display = "block";
+    }
+};
