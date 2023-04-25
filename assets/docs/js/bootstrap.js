49f3ff5ae8968196103d0cc2541a82c19913d067
Colin Wilson <colin.wilson@protonmail.ch>
2023-04-25 15:25:04 +0100

    ⚡ update bootstrap.js path

--- assets/docs/js/bootstrap.js
+++ assets/docs/js/bootstrap.js
@@ -1,6 +1,6 @@
 // Import the Bootstrap components we want to use.
 // See https://github.com/twbs/bootstrap/blob/main/js/index.umd.js
-import Tab from "/docs/js/bootstrap/src/tab";
+import Tab from "/js/bootstrap/src/tab";
 
 export default {
     Tab
